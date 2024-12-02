7bb8e362eafc077e81f85b51e42c034c64d68cb9
Mauro <mauroeavellaneda@gmail.com>
2024-12-02 21:36:01 +0100

    creates route

---
+++ auth.ts
@@ -0,0 +1,7 @@
+import NextAuth from 'next-auth'
+
+export const { getSession } = NextAuth(
+    {
+        providers: [GitHub],
+    }
+)
---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const layout = () => {
+  return (
+    <main className="font-work-sans">
+        <Navbar />
+      
+    </main>
+  )
+}
+
+export default layout
