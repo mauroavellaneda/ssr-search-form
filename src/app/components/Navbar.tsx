ab30085a43047f519c28e94c5a7c2353118c34e0
Mauro <mauroeavellaneda@gmail.com>
2024-12-02 21:56:11 +0100

    formats

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
   console.log("Navbar");
 
-  return <div>nav</div>;
+  return (
+    <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+        <Image src="/globe.svg" alt="logo" width={100} height={30} />
+        </Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
