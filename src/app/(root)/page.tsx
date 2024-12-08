285db09cf00b98de7816aaa1f763ad9cbb437b43
Mauro <mauroeavellaneda@gmail.com>
2024-12-08 18:18:24 +0100

    adds search form component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../globals.css";
+import SearchForm from "../components/SearchForm";
 
-const Home = () => {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const resolvedParams = await searchParams;
+
+  const query = resolvedParams?.query;
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <main className="flex flex-col items-center justify-center px-4 py-16"></main>
+      <section className="pink_container">
+        <h1 className="heading">Home</h1>
+        <p className="sub-heading !max-w-3xl">Welcome to the home page</p>
+        <SearchForm query={query} />
+      </section>
     </div>
   );
-};
-export default Home;
+}
