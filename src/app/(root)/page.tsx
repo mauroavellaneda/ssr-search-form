885da3a709b9187fc59fa77e450634d72303c2b2
Mauro <mauroeavellaneda@gmail.com>
2025-04-26 14:55:06 +0200

    update components dir location

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 import "../globals.css";
-import SearchForm from "../components/SearchForm";
+import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
   searchParams,
